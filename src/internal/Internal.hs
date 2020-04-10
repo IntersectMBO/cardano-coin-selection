40475199d8fc239b0baabcdebc19e71d44b942f5
Jonathan Knowles <jonathan.knowles@iohk.io>
2020-04-10 04:25:14 +0000

    Don't export submodules from `Internal`.

--- src/internal/Internal.hs
+++ src/internal/Internal.hs
@@ -10,10 +10,4 @@
 -- Types and functions defined herein are not guaranteed to be forwards or
 -- backwards compatible across different versions of the library.
 --
-module Internal
-    ( module Internal.Invariant
-    , module Internal.Rounding
-    ) where
-
-import Internal.Invariant
-import Internal.Rounding
+module Internal where
